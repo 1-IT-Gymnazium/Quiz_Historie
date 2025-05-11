89a83718b98fdf0385252c526097a2d77ed8254d
0achim <golamisme@gmail.com>
2025-05-11 19:58:46 +0200

    trochu logika + další věci(ts so hard)

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+// logika kvízu
+const quizData = [
+      { question: "otázka", options: ["1","2","3","4"], correctAnswer: "1" },
+]
 
+const QuizModule = (function() {
+  let shuffledQuestions = [];  // promíchané otázky
+  let currentIndex = 0; //index aktuální otázky
+  let score = 0; 
+  let timerInterval; //interval časovače
+  const MAX_TIME = 20; // kolik sekund na otázku
+  let timeLeft; 
+  let soundEnabled = true; // zvuk ano/ne
+
+
+ // promíchá pole
+  function shuffle(array) {
+    return [...array].sort(() => Math.random() - 0.5);
+  }
+
+})
